4f819833a194a90f5e88b8b97fd9ebaf16506140
Diego Seminario <diegoasc@gmail.com>
2018-09-13 11:18:29 -0500

    First Commit

---
+++ src/app/app.spec.protractor.js
@@ -0,0 +1,19 @@
+describe('Protractor Examples', () => {
+  it('should have title', () => {
+    browser.get('http://0.0.0.0:8080/');
+
+    const el = element(by.css('.title'));
+    
+    expect(el).toBeDefined();
+  });
+
+  it('shoud have title with value "Hello from Angular !"', () => {
+    browser.get('http://0.0.0.0:8080/');
+    const el = element(by.css('.title'));
+
+    el.getText().then((text) => {
+      expect(text).toEqual('Hello from Angular !');
+    });
+    
+  });
+});
